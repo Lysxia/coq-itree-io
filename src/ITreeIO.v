776eac8e8391a4a24e6620efb298636482dd7e6d
Li-yao Xia <lysxia@gmail.com>
2020-07-21 17:42:07 -0400

    Give it some space

--- src/ITreeIO.v
+++ src/ITreeIO.v
@@ -12,13 +12,16 @@ Instance MonadIter_IO : MonadIter IO :=
 
 (** Interpret [itree E] into [IO] given an interpreter of [E].
     This is literally a specialization of [interp]. *)
-Definition interp_io {E} : (E ~> IO) -> (itree E ~> IO) := interp.
+Definition interp_io {E} : (E ~> IO) -> (itree E ~> IO) :=
+  interp.
 Arguments interp_io {E} h [T] t.
 
 (** Interpret [itree IO] into [IO]. *)
-Definition interp_io' : itree IO ~> IO := interp (E := IO) (M := IO) (fun T (e : IO T) => e).
+Definition interp_io' : itree IO ~> IO :=
+  interp (E := IO) (M := IO) (fun T (e : IO T) => e).
 Arguments interp_io' [T] t.
 
 (** Interpret [itree void1] into [IO]. *)
-Definition interp_io_ : itree void1 ~> IO := interp (M := IO) (elim_void1 (E := IO)).
+Definition interp_io_ : itree void1 ~> IO :=
+  interp (M := IO) (elim_void1 (E := IO)).
 Arguments interp_io_ [T] t.
